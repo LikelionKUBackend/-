61c79ec58a0860f464004a3006a2e15d067aaf24
Leehongseok-code <52267586+Leehongseok-code@users.noreply.github.com>
2023-07-04 17:34:04 +0900

    commit

--- DjangoSession/mysite/pybo/urls.py
+++ DjangoSession/mysite/pybo/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 
 from . import views
 
+#하나의 프로젝트 내에 여러 개의 앱이 추가될 수 있으므로, app_name으로 이름공간을 지정
+app_name = 'pybo'
+
 urlpatterns = [
-    path('', views.index),
-    path('<int:question_id>/', views.detail),
+    path('', views.index, name = 'index'),
+    path('<int:question_id>/', views.detail, name= 'detail'),
+    path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
 ]
--- DjangoSession/mysite/pybo/views.py
+++ DjangoSession/mysite/pybo/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Question
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Question, Answer
 
 
 def index(request):
@@ -9,5 +10,13 @@ def index(request):
 
 def detail(request, question_id):
     question = Question.objects.get(id=question_id)
-    context = {'question': question}
+    answer_list = Answer.objects.filter(question_id = question_id)
+    context = {'question': question, 'answer_list': answer_list}
     return render(request, 'pybo/question_detail.html', context)
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    #form에 POST된 내용을 바탕으로 question 객체에 연결되는 answer 객체를 생성
+    answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
+    answer.save()
+    return redirect('pybo:detail', question_id=question.id)
